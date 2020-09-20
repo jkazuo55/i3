ee8990a7d4e13c3174eb334f6f84426ffc114b9e
jkazuo55 <jkazuo555@gmail.com>
2020-09-20 19:01:50 -0400

    adfafa

---
+++ moc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# mocp playing song script
+maxlen=${BLOCK_INSTANCE:-30}
+full_text="MOC not running!"
+short_text="$full_text"
+color="#505050"
+mocp -S
+mocp -a ~/Music/
+if [[ ! -z $(pidof mocp) ]]
+then
+	full_text="$(mocp -i -Q "%state%song (%artist)" | tail -n 1 | sed -e 's/PLAY//' -e 's/PAUSE//' -e 's/STOP//')"
+	short_text="${full_text:0:$maxlen}"
+	color="#90A959"
+fi
+echo "$short_text"
+echo "$full_text"
+echo "$color"
+case $BLOCK_BUTTON in
+	1) mocp -p ;; # left click = previous song
+	2) mocp -G ;; # middle click = toggle pause - play
+	3) mocp -k 7 ;; # right click = next song
+	4) mocp -r ;; # right click = next song
+	5) mocp -f ;; # right click = next song
+esac
+exit 0
